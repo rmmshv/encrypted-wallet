c7844b347eeb8c95940655f1b8300c7eef420d2c
Rimma Esheva <32555704+rmmshv@users.noreply.github.com>
2021-08-28 03:03:56 -0700

    minor code cleaning

--- wallet.js
+++ wallet.js
@@ -1,6 +1,4 @@
-
-
-const crypto = require("crypto"); // crypto standard library for key generation
+const crypto = require("crypto"); // crypto standard library 
 
 // generate public/private key pair
 const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {modulusLength: 2048, }); // 2048 is the standard secure default length for RSA keys
@@ -8,35 +6,37 @@ const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {modulusLeng
 
 // ask user for a message to encrypt
 let prompt = require("prompt-sync")();
-let inputData = prompt("Please type the message you wish to encrypt and press enter: ");
+let input_data = prompt("Please type the message you wish to encrypt and press enter: ");
 
 
 // ENCRYPT THE DATA
-let encryptedMsg = crypto.publicEncrypt(
+let encrypted_msg = crypto.publicEncrypt(
     {
         key: publicKey, // key used to encrypt the data (generated in the previous step)
         padding: crypto.constants.RSA_PKCS1_OAEP_PADDING, // OAEP padding scheme
         oaepHash: "sha256", // SHA256 hashing algorithm
     },
-    Buffer.from(inputData) // convert data string to a buffer using 'Buffer.from'
+    Buffer.from(input_data) // convert data from string to buffer format
 );
 
 // display the output
-console.log('\nYour encrypted message is:\n\n', encryptedMsg.toString("base64"), '\n');
+console.log('\nYour encrypted message is:\n\n', encrypted_msg.toString("base64"), '\n');
 
 
 // SIGN
-let signedMsg = inputData; // data to sign
+let signed_msg = input_data; // data to sign
 
-let signature = crypto.sign("sha256", Buffer.from(signedMsg), {
+// generate a signature
+let signature = crypto.sign("sha256", Buffer.from(signed_msg), {
     key: privateKey,
     padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
     }
 );
 
+// display the signature in a string format
 console.log("\nSignature:\n\n",signature.toString("base64"), '\n');
 
-let isVerified = crypto.verify("sha256", Buffer.from(signedMsg), {
+let is_verified = crypto.verify("sha256", Buffer.from(signed_msg), {
     key: publicKey,
     padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
     },
@@ -44,26 +44,26 @@ let isVerified = crypto.verify("sha256", Buffer.from(signedMsg), {
 );
 
 // if signature is verified, proceed with decryption or abort
-if (isVerified == true) {
+if (is_verified == true) {
 
-    console.log("signature verified: ", isVerified + '\n');
+    console.log("signature verified: ", is_verified + '\n');
 
     // DECRYPT THE DATA
 
-    let yesNo = prompt("Press y to decrypt the message. Press n to abort.\n");
-    if (yesNo == 'y') {
+    let yes_no = prompt("Press y to decrypt the message. Press n to abort.\n");
+    if (yes_no == 'y') {
 
-    let decryptedMsg = crypto.privateDecrypt(
+    let decrypted_msg = crypto.privateDecrypt(
     {
         key: privateKey,
         // we need to specify the same hash and padding scheme that we used to encrypt the data
         padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
         oaepHash: "sha256",
     },
-    encryptedMsg
+    encrypted_msg
     );
     // convert data from the Buffer type to a string
-    console.log("Your decrypted message is:\n\n", decryptedMsg.toString(), '\n');
+    console.log("Your decrypted message is:\n\n", decrypted_msg.toString(), '\n');
     }
     else {
      return;
